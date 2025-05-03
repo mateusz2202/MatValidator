515ac5501455ccf9a12b4b6f58244c460b1e2e8c
Mateusz Jóźwik <jmate2202@outlook.com>
2025-05-03 23:28:28 +0200

    poprawa ladowania asembly

--- MatValidator/ValidatorRegistry.cs
+++ MatValidator/ValidatorRegistry.cs
@@ -12,15 +12,32 @@ public static class ValidatorRegistry
             assemblies = [Assembly.GetCallingAssembly()];
 
 
-        var validatorTypes = assemblies.SelectMany(x => x.GetTypes()
+        var validatorTypes = assemblies
+            .SelectMany(SafeGetTypes)
             .Where(t => !t.IsAbstract && !t.IsInterface &&
                         t.BaseType != null &&
                         t.BaseType.IsGenericType &&
-                        t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>)));
+                        t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>));
 
         foreach (var validatorType in validatorTypes)
             services.AddScoped(typeof(AbstractValidator<>).MakeGenericType(validatorType.BaseType!.GetGenericArguments()[0]), validatorType);
 
         return services;
+    }
+
+    public static IEnumerable<Type> SafeGetTypes(Assembly assembly)
+    {
+        try
+        {
+            return assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException ex)
+        {
+            return ex.Types.Where(t => t != null);
+        }
+        catch
+        {
+            throw;
+        }
     }
 }
